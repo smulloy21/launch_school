9b95f6a7a286621507bbb175b9bc3193d0783d8e
Shannon Mulloy <smulloy21@gmail.com>
2025-02-05 17:43:18 -0500

    update mortgage calculator with code review feedback

--- py101/mortgage_calculator/mortgage_calculator.py
+++ py101/mortgage_calculator/mortgage_calculator.py
@@ -2,16 +2,20 @@
 A simple calculator for monthly mortgage payments
 """
 import json
+import math
 
 
 with open('messages.json', 'r', encoding='utf-8') as file:
     MESSAGES = json.load(file)
 
 
-def prompt(message, lang='en', context={}):
+def prompt(message, lang='en', context=None):
     """
     Pretty print the message in the right language, defaulting to English
     """
+    if context is None:
+        context = {}
+
     print(f'🏠 {MESSAGES[lang][message]}'.format(**context))
 
 
@@ -22,12 +26,15 @@ def get_valid_input(error_message):
     while True:
         user_input = input()
         try:
-            float(user_input)
+            number = float(user_input)
         except ValueError:
             prompt(error_message)
         else:
-            break
-    return user_input
+            if not math.isfinite(number) or math.isnan(number):
+                prompt(error_message)
+            else:
+                break
+    return number
 
 
 def get_loan_amount():
@@ -36,7 +43,7 @@ def get_loan_amount():
     """
     prompt('loan_amount')
     valid_loan_amount = get_valid_input('invalid_loan_amount')
-    return int(valid_loan_amount)
+    return valid_loan_amount
 
 
 def get_monthly_interest():
@@ -45,7 +52,7 @@ def get_monthly_interest():
     """
     prompt('apr')
     valid_apr = get_valid_input('invalid_apr')
-    return float(valid_apr) / 100 / 12
+    return valid_apr / 100 / 12
 
 
 def get_duration():
@@ -54,7 +61,38 @@ def get_duration():
     """
     prompt('duration')
     valid_duration = get_valid_input('invalid_duration')
-    return float(valid_duration) * 12
+    return valid_duration * 12
+
+
+def calculate_monthly_payment(loan_amount, monthly_interest, duration_in_months):
+    """
+    Calculate the monthly payment, accounting for 0% APR
+    """
+    try:
+        monthly_payment = loan_amount * (
+            monthly_interest /
+            (1 - (1 + monthly_interest) ** (-duration_in_months))
+        )
+    except ZeroDivisionError: # 0% interest
+        monthly_payment = loan_amount / duration_in_months
+
+    return monthly_payment
+
+
+def calculate_again():
+    """
+    Ask the user if they want to calculate again
+    """
+    while True:
+        prompt('continue')
+        answer = input().lower()
+
+        if answer.startswith('y') or answer.startswith('n'):
+            break
+
+        prompt('invalid')
+
+    return answer
 
 
 def calculate_mortgage():
@@ -65,14 +103,14 @@ def calculate_mortgage():
     loan_amount = get_loan_amount()
     monthly_interest = get_monthly_interest()
     duration_in_months = get_duration()
-    try:
-        monthly_payment = loan_amount * (
-            monthly_interest /
-            (1 - (1 + monthly_interest) ** (-duration_in_months))
-        )
-    except ZeroDivisionError: # 0% interest
-        monthly_payment = loan_amount / duration_in_months
+    monthly_payment = calculate_monthly_payment(loan_amount,
+                                                monthly_interest,
+                                                duration_in_months)
+
     prompt('result', context={"monthly_payment": monthly_payment})
+    answer = calculate_again()
+    if answer.startswith('y'):
+        calculate_mortgage()
 
 
 calculate_mortgage()
